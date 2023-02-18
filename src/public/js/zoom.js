ade9f2dcabcf40b6b6fd385d70886015782efe76
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-02-18 18:13:20 +0900

    Media Stream 주석 추가

--- src/public/js/zoom.js
+++ src/public/js/zoom.js
@@ -4,7 +4,7 @@ const myFace = document.querySelector("#myFace");
 const mute = document.querySelector("#mute");
 const camera = document.querySelector("#camera");
 
-// 영상, 소리의 데이터 스트림 (Media Stream)
+//** 영상, 소리의 데이터 스트림 (Media Stream) */
 // Media Stream API를 활용하면, Media Stream을 다룰 수 있다.
 // - Media Stream API는 WebRTC와 연관된 API이다.
 // - Media Stream API는 다음으로 구성된다.
@@ -33,7 +33,7 @@ let cameraOff = false;
 // 영상 스트림을 받아서 video 태그의 srcObject에 넣어주기
 async function getMedia() {
   try {
-    // BOM (Browser Object Model 에서 접근 가능한 객체들)
+    //** BOM (Browser Object Model 에서 접근 가능한 객체들) 에서 navigator 통해 Media Strema 추출하기 */
     // navigator 객체 : 브라우저에 대한 정보 제공
     // -> mediaDevices 프로퍼티 : 카메라, 마이크, 화면 공유 처럼 현재 연결된 미디어 입력장치에 접근할 수 있는 MediaDevices 객체 반환
     // -> MediaDevices.getUserMedia : 사용자에게 권한 요청 후, 카메라, 오디오 각각 또는 모두 활성화  => 입력 데이터를 비디오 / 오디오 트랙으로 포함한 MediaStream 객체 반환
